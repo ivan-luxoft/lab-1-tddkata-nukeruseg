65436a0dc90d82dad8a1d4b7733c6aa25ab90594
Григорян Александр <nukeruseg@gmail.com>
2020-12-07 15:45:35 +0300

    add custom delimeters implementation

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -10,11 +10,20 @@ namespace TDDKata
         private const int ERROR = -1;
         internal int Sum(string input)
         {
+            string[] delimeters = new string[] { ",", "\n" };
             if (input == null)
                 return ERROR;
             if (input.Equals(string.Empty))
                 return 0;
-            string[] arguments = input.Split(new string[] { ",", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            if (input.StartsWith("//") && input.Contains("\n"))
+            {
+                int newLineIdx = input.IndexOf("\n");
+                string delimeterFormatPart = input.Substring(0, newLineIdx);
+                string delimeter = delimeterFormatPart.Substring(2);
+                delimeters = new string[] { delimeter };
+                input = input.Substring(newLineIdx + 1, input.Length - newLineIdx - 1);
+            }
+            string[] arguments = input.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
             foreach(string argument in arguments)
             {
                 if (!int.TryParse(argument, out int intArgument) || intArgument < 0)
