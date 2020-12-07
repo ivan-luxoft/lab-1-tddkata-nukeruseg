46508ca2f45a02f73f8c8f8d4fa347e433ff8b0e
Григорян Александр <nukeruseg@gmail.com>
2020-12-07 14:40:17 +0300

    add sum implementation

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -1,14 +1,29 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using System;
+using System.Linq;
 
 namespace TDDKata
 {
     internal class StringCalc
     {
-        internal int Sum(string v)
+        private const int ERROR = -1;
+        internal int Sum(string input)
         {
-            throw new NotImplementedException();
+            if (input == null)
+                return ERROR;
+            if (input.Equals(string.Empty))
+                return 0;
+            string[] arguments = input.Split(',');
+            if (arguments.Length > 2)
+                return ERROR;
+            foreach(string argument in arguments)
+            {
+                if (!int.TryParse(argument, out int intArgument) || intArgument < 0)
+                    return ERROR;
+            }
+            var intArguments = arguments.Select(a => int.Parse(a));
+            return intArguments.Sum();
         }
     }
 }
