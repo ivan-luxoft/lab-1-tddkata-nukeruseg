65dee5212d5adbc524c82508a455a4689128498d
Григорян Александр <nukeruseg@gmail.com>
2020-12-07 14:27:10 +0300

    add test cases

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -16,5 +16,93 @@ namespace TDDKata
             int value = calc.Sum("2,2");
             Assert.That(value, Is.EqualTo(4), "Wrong actual value");
         }
+
+        [Test]
+        public void MoreThanTwoArgumentsShouldReturnError()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = "3,3,3";
+            int expected = -1;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void EmptyStringShouldReturnZero()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = string.Empty;
+            int expected = 0;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SingleArgumentShouldReturnItself()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = "3";
+            int expected = 3;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NullArgumentShouldReturnError()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = null;
+            int expected = -1;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void NegativeArgumentShouldReturnError()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = "2,-1";
+            int expected = -1;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TwoPositiveArgumentsShouldReturnSum()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = "2,2";
+            int expected = 4;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IncorrectArgumentShouldReturnError()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = "two";
+            int expected = -1;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void OneOfArgumentsIsStringShouldReturnError()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = "2,two";
+            int expected = -1;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ZeroShouldReturnZero()
+        {
+            StringCalc calc = new StringCalc();
+            string arguments = "0";
+            int expected = 0;
+            int actual = calc.Sum(arguments);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
