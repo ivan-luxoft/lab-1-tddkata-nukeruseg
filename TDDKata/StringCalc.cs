bb4d03fa0fd446e2a1ea81bf621a836b91d6a4a7
Ivan <ivan.g@inbox.ru>
2020-12-10 11:44:05 +0300

    extract method, exclude usage input variables

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -1,6 +1,8 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TDDKata
@@ -10,27 +12,68 @@ namespace TDDKata
         private const int ERROR = -1;
         internal int Sum(string input)
         {
+            var argumentsForParse = input;
             string[] delimeters = new string[] { ",", "\n" };
-            if (input == null)
+
+            if (argumentsForParse == null)
                 return ERROR;
-            if (input.Equals(string.Empty))
+
+            if (argumentsForParse.Equals(string.Empty))
                 return 0;
-            if (input.StartsWith("//") && input.Contains("\n"))
-            {
-                int newLineIdx = input.IndexOf("\n");
-                string delimeterFormatPart = input.Substring(0, newLineIdx);
-                string delimeter = delimeterFormatPart.Substring(2);
-                delimeters = new string[] { delimeter };
-                input = input.Substring(newLineIdx + 1, input.Length - newLineIdx - 1);
-            }
-            string[] arguments = input.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
-            foreach(string argument in arguments)
+
+            argumentsForParse = ExtractCustomDelimeter(argumentsForParse, ref delimeters);
+
+            var intArguments = ConvertArgumentStringToIntArray(argumentsForParse, delimeters);
+
+            if(intArguments == null)
+                return ERROR;
+
+            return intArguments.Sum();
+        }
+
+        /// <summary>
+        /// Разделяет строку по переданным разделителям и возвращает список чисел
+        /// </summary>
+        /// <param name="argumentsForParse">Строка для разделения</param>
+        /// <param name="delimeters">Массив разделителей</param>
+        /// <returns>Список чисел (полученных из строки) или NULL в случае ошибки</returns>
+        private static List<int> ConvertArgumentStringToIntArray(string argumentsForParse, string[] delimeters)
+        {
+            var result = new List<int>();
+            string[] arguments = argumentsForParse.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string argument in arguments)
             {
                 if (!int.TryParse(argument, out int intArgument) || intArgument < 0)
-                    return ERROR;
+                    result.Add(intArgument);
             }
-            var intArguments = arguments.Select(a => int.Parse(a));
-            return intArguments.Sum();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Выделяет настраиваемый разделитель и возвращает строку содержающую только аргументы
+        /// </summary>
+        /// <param name="argumentsForParse">Строка с настраиваемым разделителем и аргументами</param>
+        /// <param name="delimeters">Массив разделителей, в него будет записан настраиваемый, если он есть в строке</param>
+        /// <returns>Входная строка argumentsForParse очищенная от настроек разделителя, только аргументы</returns>
+        private static string ExtractCustomDelimeter(string argumentsForParse, ref string[] delimeters)
+        {
+            var result = argumentsForParse;
+            if (result.StartsWith("//") && result.Contains("\n"))
+            {
+                int newLineIdx = result.IndexOf("\n");
+                string delimeterFormatPart = result.Substring(0, newLineIdx);
+                string delimeter = delimeterFormatPart.Substring(2);
+
+                delimeters = new string[] { delimeter };
+
+                var copyStringFrom = newLineIdx + 1;
+                var copyStringLength = argumentsForParse.Length - newLineIdx - 1;
+                return result.Substring(copyStringFrom, copyStringLength);
+            }
+
+            return result;
         }
     }
 }
